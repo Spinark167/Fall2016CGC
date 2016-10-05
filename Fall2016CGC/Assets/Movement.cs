e0aee041331cb87c0d15cb46e23dd11a9f913275
Spinark167 <jhandrews1@gmail.com>
2016-10-04 21:16:42 -0400

    Added an example main character and ground
    Using some of the imported assets, I added a robot guy that I then gave
    a movement script to. This script is not complete. I also added a ground
    to act as an environment to move in.

---
+++ Fall2016CGC/Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float moveSpeed = 15.0f;
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			rb.velocity = new Vector3 (-moveSpeed, rb.velocity.y, 0f);
+		}
+	}
+}
