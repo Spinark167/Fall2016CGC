ef57afbc03b07edf4cad4e421ec9c50124725f53
Spinark167 <jhandrews1@gmail.com>
2016-10-10 14:29:19 -0400

    Post 30 minutes of Gucci Devs
    Ok so I asked y'all to watch thirty minutes of videos and I did the
    same. I followed along in here so that we can all see whats happening. I
    also did some modificaitons that we will be using. I'm going to go over
    stuff on Tuesday.

---
+++ Fall2016CGC/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+	private Movement movement;
+
+	// Use this for initialization
+	void Start () {
+		movement = gameObject.GetComponentInParent<Movement> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		movement.grounded = true;
+	}
+
+	void OnTriggerStay2D(Collider2D col){
+		movement.grounded = true;
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		movement.grounded = false;
+	}
+}
---
+++ Fall2016CGC/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float moveSpeed = 50.0f;
+	public float jumpPower = 150.0f;
+
+	Rigidbody2D rb;
+
+	public bool grounded;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Horizontal") < -.1f) {
+			transform.localScale = new Vector3 (-1, 1, 1);
+		}
+
+		if (Input.GetAxis ("Horizontal") > .1f) {
+			transform.localScale = new Vector3 (1, 1, 1);
+		}
+
+		if ((Input.GetButtonDown ("Jump") || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && grounded) {
+			rb.velocity = new Vector2 (rb.velocity.x, jumpPower);
+		}
+	}
+
+	void FixedUpdate(){
+		float h = Input.GetAxis ("Horizontal");
+		rb.velocity = new Vector2 (moveSpeed * h, rb.velocity.y);
+	}
+}
