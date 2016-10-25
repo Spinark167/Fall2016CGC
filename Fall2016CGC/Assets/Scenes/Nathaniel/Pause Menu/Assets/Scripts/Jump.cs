cabeb0cf49ef9b41ab4df3ec9ef84c6b8c70bd3b
nathanielmills <njmills@email.sc.edu>
2016-10-25 01:49:58 -0400

    Added Pause Menu
    Added pause menu assets and scene to Nathaniel Scene folder

---
+++ Fall2016CGC/Assets/Scenes/Nathaniel/Pause Menu/Assets/Scripts/Jump.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+    public float speed = 50f;
+    public float jumpPower = 150f;
+
+    public bool grounded;
+    private Rigidbody2D rb2d;
+
+	void Start ()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+	
+	}
+}
