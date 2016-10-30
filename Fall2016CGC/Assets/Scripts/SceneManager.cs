fa47faba03702b0127e6d7d7427d5639a5c862d0
nathanielmills <njmills@email.sc.edu>
2016-10-30 13:20:15 -0400

    Added SceneManager script
    SceneManager script was added

---
+++ Fall2016CGC/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+    static SceneManager Instance;
+
+	// Use this for initialization
+	void Start () {
+        if (Instance != null)
+        {
+            GameObject.Destroy(gameObject);
+        }
+        else
+        {
+            GameObject.DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyUp(KeyCode.Keypad1))
+        {
+            Application.LoadLevel("StartScene");
+        }
+    if (Input.GetKeyUp(KeyCode.Keypad2))
+        {
+            Application.LoadLevel("Scene1.1")
+        }
+	}
+}
