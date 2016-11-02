295c182e729b9063139772dfafd0f2b4932c70a3
nathanielmills <njmills@email.sc.edu>
2016-11-01 20:14:08 -0400

    Made log creater and log up script
    game object that will control log creation and script that controls log
    life time and movement

---
+++ Fall2016CGC/Assets/Scripts/MovingLogsUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingLogsUp : MonoBehaviour {
+
+    // Use this for initialization
+    float lifeTimer = 6f;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifeTimer -= Time.deltaTime;
+        if (lifeTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+        transform.Translate(new Vector2(0f, (Time.deltaTime)*5));
+	}
+}
