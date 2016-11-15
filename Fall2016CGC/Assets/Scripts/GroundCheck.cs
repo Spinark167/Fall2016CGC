1a8ade997db200f35de2d533c293c77a23eb3712
Spinark167 <jhandrews1@gmail.com>
2016-11-15 14:04:24 -0500

    Fixed the main character's colliders. Added several new sprites. Updated the ground checker script. Gave all the monsters slippery colliders
    For the main character, I gave him a small circle collider at his base
    so that he won't get caught on side by side box colliders incase there
    is ever a case of that. I then added sprites for the title screen and
    for the elvated path. I also added big green blocks to fill gaps.
    Lastly, on the ground checker script, I made it where grounded wouldn't
    become true unless your y velocity is equal to zero, this will stop the
    player from being able to jump of off walls when in motion. Lastly, to
    stop the bee from getting stuck on other enemies, I made all of the
    monster colliders slippery.

--- Fall2016CGC/Assets/Scripts/GroundCheck.cs
+++ Fall2016CGC/Assets/Scripts/GroundCheck.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class GroundCheck : MonoBehaviour {
 
 	private Movement movement;
+	private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
 		movement = gameObject.GetComponentInParent<Movement> ();
+		rb = gameObject.GetComponentInParent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
@@ -16,13 +18,13 @@ public class GroundCheck : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D col){
-		if (col.gameObject.tag == "Ground") {
+		if (col.gameObject.tag == "Ground" && rb.velocity.y == 0) {
 			movement.grounded = true;
 		}
 	}
 
 	void OnTriggerStay2D(Collider2D col){
-		if (col.gameObject.tag == "Ground") {
+		if (col.gameObject.tag == "Ground" && rb.velocity.y == 0) {
 			movement.grounded = true;
 		}
 	}
